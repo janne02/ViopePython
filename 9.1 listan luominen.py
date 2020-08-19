ca8fb4f128d71880720712b6548cd24d9b5f79c5
Janne02 <35040807+janne02@users.noreply.github.com>
2020-08-19 16:42:39 +0300

    Create 9.1 listan luominen.py

---
+++ 9.1 listan luominen.py
@@ -0,0 +1,9 @@
+def main():
+    list=["Sininen","Punainen","Keltainen","Vihreä"]
+    print("Listan ensimmäinen alkio on:", list[0])
+    print("Lista tulostettuna alkio kerrallaan:")
+    for i in list:
+        print(i)
+
+if __name__ == "__main__":
+    main()
