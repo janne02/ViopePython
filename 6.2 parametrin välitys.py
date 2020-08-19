f5eb6743c921603535e87c3fe328ce83fa9e5d1c
Janne02 <35040807+janne02@users.noreply.github.com>
2020-08-19 16:38:14 +0300

    Create 6.2 parametrin välitys.py

---
+++ 6.2 parametrin välitys.py
@@ -0,0 +1,9 @@
+def tulostusfunktio(luku):
+	print("Toinen potenssi on",luku**2)
+
+def main():
+	annettu_luku = int(input("Anna lukuarvo: "))
+	tulostusfunktio(annettu_luku)
+	
+if __name__ == "__main__":
+	main()
