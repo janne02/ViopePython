0bd063287c2511606d367b644a310d7576d2d60e
Janne02 <35040807+janne02@users.noreply.github.com>
2020-08-19 16:38:47 +0300

    Create 6.4 paluuarvojen hyödyntäminen.py

---
+++ 6.4 paluuarvojen hyödyntäminen.py
@@ -0,0 +1,18 @@
+ef main():
+	syote = input("Anna syöte (Lopeta lopettaa): ")
+	pituus(syote)
+
+
+def pituus(merkkijono):
+	if merkkijono == "Lopeta":
+		exit()
+	elif len(merkkijono) == 0:
+		print("Et antanut syötettä")
+		main()
+	else:
+		print("Antamasi syöte oli",len(merkkijono),"merkkiä pitkä.")
+		main()
+
+	
+if __name__ == "__main__":
+	main()
