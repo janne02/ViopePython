33dc09bbc35a61f331e3445c2847de8e717fbec8
Janne02 <35040807+janne02@users.noreply.github.com>
2020-08-19 16:29:13 +0300

    Create 3.2 if-else-rakenne.py

---
+++ 3.2 if-else-rakenne.py
@@ -0,0 +1,8 @@
+if input("Anna nimi:") == "Erkki":
+	juomas = input("Anna salasana:")
+	
+	if juomas == "Esimerkki":
+		print("Salasana ja nimi oli oikein!")
+	else: print("Salasana oli väärin.")
+
+else: print("Nimi oli väärin.")
