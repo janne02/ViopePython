e8314de0d3dcf07ee49625ef66199930ddabd330
Janne02 <35040807+janne02@users.noreply.github.com>
2020-08-19 16:30:00 +0300

    Create 3.3 if-elif-else-rakenne.py

---
+++ 3.3 if-elif-else-rakenne.py
@@ -0,0 +1,10 @@
+pelaaja = int(input("Valitse kohde (1-3): "))
+
+if(pelaaja == 1):
+    print("Haukion kala Oy")
+elif(pelaaja == 2):
+    print("Metallipaja VasaraAika")
+elif(pelaaja == 3):
+    print("Balin palapelitehdas")
+else:
+    print("Olet valinnut jotain muuta.")
