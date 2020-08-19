3c1b66414f1e0bdcaba3005c6904b6fcff20c175
Janne02 <35040807+janne02@users.noreply.github.com>
2020-08-19 16:27:58 +0300

    Create 2.6 Leikkaus.py

---
+++ 2.6 Leikkaus.py
@@ -0,0 +1,4 @@
+a="Hattukauppias"
+print("Muuttujan 4 ensimmäistä kirjainta ovat", a[0]+a[1]+a[2]+a[3])
+print("Muuttujan 4 viimeistä kirjainta ovat", a[-4]+a[-3]+a[-2]+a[-1])
+print("Muuttujan teksti on väärinpäin", a[::-1])
